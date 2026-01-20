9546e3e528c0cc5d45fc88d92781b84ea47bbf42
themo99 <theomortimer2003@gmail.com>
2026-01-20 23:40:12 +1300

    Added comments and string cleaning/extraction

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+import re
+
 prices = [3.50, 2.99, 5.25, 4.10]
 
-sum_prices = round(sum(prices), 2)
+sum_prices = round(sum(prices), 2) # get the total price
 
 print(sum_prices) ## Q1
 
@@ -9,7 +11,7 @@ cheap_items = []
 
 for i in range(len(prices)):
     if prices[i] < 4:
-        cheap_items.append(prices[i])
+        cheap_items.append(prices[i]) # find cheap items
 
 print(cheap_items)
 
@@ -19,11 +21,11 @@ freq_list = []
 num_list = []
 pairings_dict = {}
 
-for i in range(len(nums)):
+for i in range(len(nums)):       # get a unique list of items
     if nums[i] not in num_list:
         num_list.append(nums[i])
 
-for i in range(len(num_list)):
+for i in range(len(num_list)):               # find the frequency of each item
     pairings_dict[num_list[i]] = 0
     for j in range(len(nums)):
         if num_list[i] == nums[j]:
@@ -33,7 +35,7 @@ print(pairings_dict) ## Q3
 
 def price_check(price1, price2):
     """determine the price of item2 relative to item 1"""
-    if price1 < price2:
+    if price1 < price2:                                       # find the relative prices
         return f'${price1} is cheaper than ${price2}'
     elif price1 == price2:
         return f'The items are the same cost (${price1})'
@@ -48,7 +50,7 @@ items = ["milk", "bread", "milk", "eggs", "bread"]
 
 unique_items = []
 
-for item in items:
+for item in items:                    # get a unique shopping list
     if item not in unique_items:
         unique_items.append(item)
 
@@ -58,12 +60,56 @@ nums2 = [1, 2, 3, 4]
 
 running_total = [0] * len(nums2)
 
-for i in range(len(nums2)):
+for i in range(len(nums2)):                  # get a running total as each item is added
     running_total[i] += sum(nums2[:i + 1])
 
-print(running_total) # Q5
+print(running_total) # Q6
 
 tricky_string = "$6.49 per kg"
-price = tricky_string.replace(/\D/g,'');
+return_price = ''
 
-print(price)
+for letter in tricky_string:                                                # extract prices
+    if letter in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']:
+        return_price += letter
+
+print(float(return_price)) # Q7
+
+ugly_string = "  nz BEEF Mince  "
+clean_string = ''
+
+ugly_words = ugly_string.rsplit()  # normalize string
+for word in ugly_words:
+    word = word.capitalize()
+    if word == 'Nz':
+        word = 'NZ'
+    clean_string += word + ' '
+
+print(clean_string) # Q8
+
+items2 = {
+    "milk": 3.50,
+    "bread": 2.99,
+    "eggs": 6.20
+}
+
+items2_total = round(sum(items2.values()), 2)  # create a 'recipet'
+items2['Total'] = items2_total
+
+for product, price in items2.items():
+    print(f'{product:<5} ${price:.2f}')     
+
+#for set in items2:
+
+paknsave = {"milk": 3, "bread": 3}
+countdown = {"milk": 4, "bread": 1}
+newworld = {"milk": 5, "bread": 4}
+
+cheapest_products = paknsave
+
+for product, price in countdown.items():
+    for low_product, low_price in cheapest_products.items():
+        if countdown[product] == cheapest_products[product] and price < low_price:
+            cheapest_products[low_price] = countdown[price]
+
+for product, price in cheapest_products.items():
+    print(f'{product:} → (${price:.2f})') 
